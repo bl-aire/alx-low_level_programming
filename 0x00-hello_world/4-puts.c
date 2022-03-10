60b42f36a8ff312b87cdafa648cb39994f27a91b
Blaire <ogoh_blessing@yahoo.com>
2022-03-10 13:24:40 -0800

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main is the Entry point
+ *
+ * Return 0 (Succss) always
+ * /
+int main(voud)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
