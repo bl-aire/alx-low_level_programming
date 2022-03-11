066f7cad2fed170f03abf31d2b91946d067165e9
Blaire <ogoh_blessing@yahoo.com>
2022-03-11 08:53:44 -0800

    Last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("")
+if(n > 5){}
+else if(n == 0){}
+else if(n < 6 && != 0){}
+return (0);
+}
