e50a759a91b6ccaea8f32b09834c846290fa31a1
Blaire <ogoh_blessing@yahoo.com>
2022-03-21 15:34:19 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts(char *str): prints string followed by new line to stdout
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; *(str + i) != '\0'; i++)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
+}
