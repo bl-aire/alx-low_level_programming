31b84333dd7b183f3eeeb624eef35aafefc5050b
Blaire <ogoh_blessing@yahoo.com>
2022-03-22 02:53:04 +0100

    Put half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 #include "2-strlen.c"
 /**
@@ -14,32 +13,9 @@ if (_strlen(str) % 2 != 0)
 {
 oE += 1;
 }
-for (i = (_strlen(str) + oE) / 2; i < _strlen(str);i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
-}
-=======
-#include "main.h"
-#include "2-strlen.c"
-/**
- *puts_half - puts half of int
- *
- *@str: string to be split
- */
-void puts_half(char *str)
-{
-int i;
-int oE = 0;
-if (_strlen(str) % 2 != 0)
-{
-oE += 1;
-}
 for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
 {
 _putchar(str[i]);
 }
 _putchar('\n');
 }
->>>>>>> d0315f29b111fd1ca0244f4030a43fbbc40ee8ff
